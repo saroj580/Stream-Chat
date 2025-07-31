7b01f3a592927f37cfa5f609a0c4b9d347fca416
saroj580 <chaudharysaroj819@gmail.com>
2025-07-31 15:28:00 +0530

    create a user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required : true
+    },  
+    email: {
+        type: String,
+        required: true,
+        unique : true
+    }, 
+    password: {
+        type: String,
+        required: true,
+        minlength : 6
+    },
+    bio: {
+        type: String,
+        default : "",
+    },
+    profilePic: {
+        type: String,
+        default : ""
+    },
+    nativeLanguage: {
+        type: String,
+        default : ""
+    },
+    learningLanguage: {
+        type: String,
+        default : ""
+    },
+    location: {
+        type: String,
+        default : ""
+    },
+    isOnboarded: {
+        type: Boolean,
+        default : false
+    },
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+// pre hook
+//Just before to save the user in database, hash their password
+userSchema.pre('save', async function(next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+})
+
+export default User;
