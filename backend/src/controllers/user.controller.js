6b98ccab0e0252b1349774e51a4ac9c90eabad3e
saroj580 <chaudharysaroj819@gmail.com>
2025-08-05 20:28:14 +0530

    create accept controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -73,3 +73,38 @@ export async function sendFriendRequest(req, res) {
         console.log("Error in sendFriendRequest", error.message);
         res.status(500).json({ message: "Internal server error" });    }
 }
+
+export async function acceptFriendRequest(req, res) {
+    try {
+        const { id: requestId } = req.params;
+
+        const friendReq = await FriendRequest.findById(requestId);
+
+        if (!friendReq) {
+            return res.status(404).json({ message: "Friend request not found" });
+        }
+
+        //verify the current user is recipient id
+        if (friendReq.recipient.toString() !== req.user.id) {
+            return res.status(403).json({ message: "You're not authorized to accept this friend request" });
+        }
+
+        friendReq.status = "accepted";
+        await friendReq.save();
+
+        // add each user to other's friends array
+        // $addToSet : adds element to an array only if they don't already exist.
+        await User.findByIdAndUpdate(friendReq.sender, {
+            $addToSet: { friends: friendReq.recipient }
+        });
+
+        await User.findByIdAndUpdate(friendReq.recipient, {
+            $addToSet: { friends: friendReq.sender }
+        });
+
+        res.status(200).json({ message: "Friend request accepted" });
+    } catch (error) {
+        console.log("Error in acceptFriendRequest", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getMyFriends, getRecomendedUsers, sendFriendRequest } from '../controllers/user.controller.js';
+import { acceptFriendRequest, getMyFriends, getRecomendedUsers, sendFriendRequest } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.use(protectRoute);
 router.get('/', getRecomendedUsers);
 router.get('/friends', getMyFriends);
 router.get('/friend-request/:id',  sendFriendRequest);
+router.put('/friend-request/:id/accept',  acceptFriendRequest);
 
 
 export default router;
